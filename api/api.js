d9fd46d0ce51638eece8ecfc3e114c07ec3d1f4f
Paul Kiunisala <paulchristiank@gmail.com>
2018-05-18 16:38:51 +0800

    added source files from react-firebase-authentication tutorial

--- api/api.js
+++
@@ -1,1 +0,0 @@
-const apiKey = "AIzaSyCO3b5rD3tihXWtAHp_R6GD35qsj9OC4r8"
--- public/resize.js
+++
@@ -1,9 +0,0 @@
-var onResize = function() {
-	$("body").css("padding-top", $(".navbar-fixed-top").height());
-};
-
-$(window).resize(onResize);
-
-$(function() {
-	onResize();
-});
