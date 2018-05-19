b7dc2d3cfa641190532391c30a1301bab137d5b4
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2018-05-19 16:25:27 +0800

    added maps with a directions renderer

--- src/components/RightNav.js
+++ src/components/RightNav.js
@@ -1,38 +1,25 @@
 import React, { Component } from 'react';
-<<<<<<< Updated upstream
-
-<<<<<<< HEAD
-=======
 import Maps from '../containers/Maps';
->>>>>>> Stashed changes
 
-class RightNav extends Component {
-	render() {
-		return(
-            <div className="col-lg-2">
-                <div className="panel-group">
-                    <div className="panel panel-default">
-                        <div className="panel-heading">
-                            MAPS
-                        </div>
-                    </div>
-                </div>
-            
-                <Maps />
-            </div>
-        )
-=======
 class RightNav extends Component {	
 	render() {
+		// console.log("Yo");
+		// console.log(this.props);
+		// console.log("Yo");
 		if(!this.props.loaded) {
-			return <div>Loading... </div>
+			return (
+				<div className="col-lg-2">
+					<Maps />
+				</div>
+				// <div> ...Loaded</div>
+			)
 		}
 		return(
 			<div>			
 			</div>
 		)
->>>>>>> master
 	}
 }
+
 
 export default RightNav;
--- src/containers/Maps.js
+++ src/containers/Maps.js
@@ -1,39 +1,74 @@
-import React, {Component} from 'react';
-import apiKey from '../../api/api.js';
-import { GoogleApiWrapper, Map, Marker, InfoWindow } from 'google-maps-react';
+import React, { Component } from 'react';
+import { compose, withProps, lifecycle } from 'recompose';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, DirectionsRenderer } from "react-google-maps";
 
-export class Maps extends Component {
+            
+const MapWithADirectionsRenderer = compose(
+    withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCO3b5rD3tihXWtAHp_R6GD35qsj9OC4r8&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `400px` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+    }),
+    withScriptjs,
+    withGoogleMap,
+        lifecycle({
+            componentDidMount() {
+                const google=window.google;
+                const DirectionsService = new google.maps.DirectionsService();
+                var originlat = null;
+                var originlng = null;
+                var error = null;
 
-    constructor() {
-        super();
-        this.state = {
-            selectedPlace: {
-                name: ""
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        originlat = position.coords.latitude;
+                        originlng = position.coords.longitude;
+                        error = null;
+
+                        DirectionsService.route({
+                            origin: new google.maps.LatLng(originlat, originlng),
+                            destination: new google.maps.LatLng(10.3186, 123.9005),
+                            travelMode: google.maps.TravelMode.DRIVING,
+                        }, (result, status) => {
+                            if (status === google.maps.DirectionsStatus.OK) {
+                            this.setState({
+                                directions: result,
+                            });
+                            } else {
+                            console.error(`error fetching directions ${result}`);
+                            }
+                        });
+                    },
+                    (error) => error = error.message,
+                    { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },    
+                )
             }
-        }
-    }
+        })
+    )(props =>
+    <GoogleMap
+        defaultZoom={7}
+        defaultCenter= {{lat: -34.397, lng: 150.644}} //{new google.maps.LatLng(41.8507300, -87.6512600)}
+    >
+        {props.directions && <DirectionsRenderer directions={props.directions} />}
+    </GoogleMap>
+);
 
+function DirectionsWrapper(props) {
+
+
+}
+
+
+
+class Maps extends Component {    
+   
     render() {
-        const style = {
-            width: '100vw',
-            height: '100vh'
-        }
+
         return (
-            <Map google={this.props.google} zoom={14} style={style}>
-
-                <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
-
-                <InfoWindow onClose={this.onInfoWindowClose}>
-                    <div>
-                        <h1>{this.state.selectedPlace.name}</h1>
-                    </div>
-                </InfoWindow>
-            </Map>
+            <MapWithADirectionsRenderer />
         )
     }
 }
 
-export default GoogleApiWrapper({
-    apiKey: apiKey.api,
-})(Maps);
+export default Maps;
