39f4e0e96586083651a7af5b76024bb2f690f563
Juan Carlos Roldan <jtroldan@up.edu.ph>
2018-05-19 17:05:13 +0800

    Resolve minor merge conflict

--- src/components/RightNav.js
+++ src/components/RightNav.js
@@ -2,17 +2,10 @@ import React, { Component } from 'react';
 import '../stylesheets/MainPage.css';
 
 class RightNav extends Component {	
-	render() {		
-		if(!this.props.loaded) {
-			return (
-				<div className="col-lg-2">
-					<Maps />
-				</div>
-				// <div> ...Loaded</div>
-			)
-		}
+	render() {				
 		return(
-			<div>			
+			<div>	
+				Loading...		
 			</div>
 		)
 	}
