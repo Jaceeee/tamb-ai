dea2367dca6663dc5c86f406916e95afbdefe139
Juan Carlos Roldan <jtroldan@up.edu.ph>
2018-05-21 00:54:53 +0800

    Fix Comment Tab

--- src/components/CommentTab.jsx
+++ src/components/CommentTab.jsx
@@ -58,8 +58,6 @@ const CommentItems = (props) => {
     return commentItem.place_id === placeId;
   });
 
-  console.log(filteredCommentsByPlace);
-
   const commentsList = filteredCommentsByPlace.map((commentItem) => {
     let user;
 
@@ -72,10 +70,7 @@ const CommentItems = (props) => {
 
     return (<CommentItem context={commentItem.context}
                         user={user} />);
-  });
-  
-  console.log("Comments list");
-  console.log(commentsList);
+  });  
 
   return (
     <ul className="CommentList">
